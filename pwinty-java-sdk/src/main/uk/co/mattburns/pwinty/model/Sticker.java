df7617548160fff46f1378cce88732114ba1db7f
Matt Burns <matt@mattburns.co.uk>
2012-05-15 13:13:13 +0100

    Another attempt at a first commit.

---
+++ pwinty-java-sdk/src/main/uk/co/mattburns/pwinty/model/Sticker.java
@@ -0,0 +1,21 @@
+package uk.co.mattburns.pwinty.model;
+
+public class Sticker {
+
+    private Integer id;
+    private String fileName;
+    private int orderId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+}
