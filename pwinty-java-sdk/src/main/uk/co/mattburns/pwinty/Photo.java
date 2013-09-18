b30a42b31f6f690de1420a38ccadac5d013a95cd
Matt Burns <matt@mattburns.co.uk>
2013-09-18 10:24:29 +0100

    Removed sticker as a valid photo type.

--- pwinty-java-sdk/src/main/uk/co/mattburns/pwinty/Photo.java
+++ pwinty-java-sdk/src/main/uk/co/mattburns/pwinty/Photo.java
@@ -13,7 +13,7 @@ public class Photo {
     private int orderId;
 
     public enum Type {
-        sticker, _4x4, _4x6, _5x7, _6x6, _8x8, _8x10, _8x12, _4x18, _P16x24, _P18x18, _P18x24, _P20x30, _P24x24, _P24x36;
+        _4x4, _4x6, _5x7, _6x6, _8x8, _8x10, _8x12, _4x18, _P16x24, _P18x18, _P18x24, _P20x30, _P24x24, _P24x36;
 
         @Override
         public String toString() {
