c8d99f9e5e0877757236309f370d16f954e20dc6
Matt Burns <matt@mattburns.co.uk>
2014-06-20 12:27:29 +0100

    Added new print size and handle case when tracking url is empty string.

--- pwinty-java-sdk/src/main/uk/co/mattburns/pwinty/v2/ShippingInfo.java
+++ pwinty-java-sdk/src/main/uk/co/mattburns/pwinty/v2/ShippingInfo.java
@@ -1,29 +1,38 @@
 package uk.co.mattburns.pwinty.v2;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ShippingInfo {
     private boolean isTracked;
     private String trackingNumber;
-    private URL trackingUrl;
+    private String trackingUrl;
     private int price;
-    
+
     public boolean isTracked() {
         return isTracked;
     }
-    
+
     public String getTrackingNumber() {
         return trackingNumber;
     }
-    
+
     public URL getTrackingUrl() {
-        return trackingUrl;
+        if (trackingUrl != null && !trackingUrl.isEmpty()) {
+            try {
+                return new URL(trackingUrl);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
     }
-    
+
     public int getPrice() {
         return price;
     }
-    
+
     @Override
     public String toString() {
         return "ShippingInfo [isTracked=" + isTracked + ", trackingNumber="
